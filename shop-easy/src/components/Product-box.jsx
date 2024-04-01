4a4d350dc7a27a1c21c5dec04322761f57284502
Henrique <henrynyyls@gmail.com>
2024-04-01 17:35:05 -0300

    New verification function added
    Now it verifies which product type I clicked on. Ex: Spices, Meats, Fruits and Vegetables or Cleaning Products(for now)

--- shop-easy/src/components/Product-box.jsx
+++ shop-easy/src/components/Product-box.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import '../App.css'
-import meat1 from '../images/meat1.png'
 
-function ProductBox(){
+function ProductBox({imageLink}){
   return (
     <nav className='product-box'>
       <div className='container-2'>
-        <img className='product-image' src={meat1} alt="product's image" />
+        <img className='product-image' src={imageLink} alt="product's image" />
         <h1 className='product-name'>Picanha</h1>
         <p className='product-description'>
           Carne de altíssima qualidade, com incrível sabor e altos níveis de nutrientes.
--- shop-easy/src/components/Products.jsx
+++ shop-easy/src/components/Products.jsx
@@ -1,30 +1,36 @@
-import React from 'react'
+import {React, useEffect, useState } from 'react'
 import '../App.css'
 import ProductBox from './Product-box.jsx'
-
+import meat1 from '../images/meat1.png'
 
 function Products(){
+  let opcoes = ['spices', 'meats', 'fruitsAndVegetables', 'cleaningProducts'];
+  const [products, setProducts] = useState('');
+  console.log(products)
+  const [itens, setItens] = useState([]);
+
+  function clicked(param){
+    opcoes.map((value,index) => {
+      if(param === value){
+        setProducts(param)
+      }
+    })
+  }
+
   return <>
     <nav className='products'>
       <div className='container-1'>
         <h1>Products</h1>
         <ul className='products-categories'>
-          <li>Spices</li>
-          <li>Meats</li>
-          <li>Fruits And Vegetables</li>
-          <li>Cleaning Products</li>
+          <button onClick={() => clicked('spices')}>Spices</button>
+          <button onClick={() => clicked('meats')}>Meats</button>
+          <button onClick={() => clicked('fruitsAndVegetables')}>Fruits And Vegetables</button>
+          <button onClick={() => clicked('cleaningProducts')}>Cleaning Products</button>
         </ul>
       </div>
       <div className='container-2 row'>
-        <ProductBox></ProductBox>
-        <ProductBox></ProductBox>
-        <ProductBox></ProductBox>
-        <ProductBox></ProductBox>
-        <ProductBox></ProductBox>
-        <ProductBox></ProductBox>
-        <ProductBox></ProductBox>
-        <ProductBox></ProductBox>
-        <ProductBox></ProductBox>
+          <ProductBox imageLink={meat1}/>
+          <ProductBox imageLink={meat1}/>
       </div>
     </nav>
   </>
